9ccf369754eddd15ba568bd0ae2367b28bc549c4
Mike Leonard <mike.r.leonard@gmail.com>
2012-08-03 16:32:27 +1000

    Completed Book 2, Lab 5 - Reflection.

---
+++ JavaBeanSamples/src/com/webagesolutions/reflection/Target.java
@@ -0,0 +1,14 @@
+package com.webagesolutions.reflection;
+
+public class Target
+{
+  public void sayHello()
+  {
+    System.out.println("-> Hello.");
+  }
+  
+  public void sayGoodbye()
+  {
+    System.out.println("-> Goodbye.");
+  }
+}
